a0f65e2323caababfe3b48b6787e1cb584e2b5c6
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-16 16:06:07 +0100

    add exercise 1 on js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*
+Esercizio 1
+
+Dichiara una variabile chiamata number e assegnale il valore 100. 
+Stampa il valore della variabile in console.
+
+*/
+
+const number = 100; // number
+
+console.log(number);
